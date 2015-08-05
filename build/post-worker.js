0ca5bd3dbe61701f79119a84f6d681f2706fc168
Kagami Hiiragi <kagami@genshiken.org>
2015-08-05 18:06:00 +0300

    Transfer MEMFS buffers

--- build/post-worker.js
+++ build/post-worker.js
@@ -5,19 +5,18 @@ var __ffmpegjs_running = false;
 
 self.onmessage = function(e) {
   var msg = e.data;
-  var opts, key, result;
   if (msg["type"] == "run") {
     if (__ffmpegjs_running) {
       self.postMessage({"type": "error", "data": "already running"});
     } else {
       __ffmpegjs_running = true;
       self.postMessage({"type": "run"});
-      opts = {};
-      for (key in msg) {
+      var opts = {};
+      Object.keys(msg).forEach(function(key) {
         if (key !== "type") {
           opts[key] = msg[key]
         }
-      }
+      });
       opts["stdin"] = function() {
         // NOTE(Kagami): Since it's not possible to pass stdin callback
         // via Web Worker message interface, set stdin to no-op. We are
@@ -34,8 +33,11 @@ self.onmessage = function(e) {
       };
       // TODO(Kagami): Should we wrap this function into try/catch in
       // case of possible exception?
-      result = __ffmpegjs(opts);
-      self.postMessage({"type": "done", "data": result});
+      var result = __ffmpegjs(opts);
+      var transfer = result["MEMFS"].map(function(file) {
+        return file["data"].buffer;
+      });
+      self.postMessage({"type": "done", "data": result}, transfer);
       __ffmpegjs_running = false;
     }
   } else {
